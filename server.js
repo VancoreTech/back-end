655ad2672ac841b64610a7e18a39f5cfdfe1f068
sirhmvfx <sirhmvfx@gmail.com>
2025-06-10 13:08:18 +0100

    create server with auth routes

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const cors = require("cors");
 const passport = require("passport");
 const app = express();
 
+const authRoutes = require("./src/routes/auth.routes");
+
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
