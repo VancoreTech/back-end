26ca039bf7cbc2aa92987731aa2ab6c838f64605
sirhmvfx <sirhmvfx@gmail.com>
2025-06-14 08:16:14 +0100

    added route for individual checks

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { register, verifyEmail } from "../controllers/auth.controller.js";
+import { checkBusinessName, checkEmail, checkStoreUrl, checkUsername, login, register, verifyEmail } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-
-router.post("/register", register)
+router.post("/check-username", checkUsername)
+router.post("/check-email", checkEmail)
 router.post("/verify-email", verifyEmail)
+router.post("/check-business-name", checkBusinessName)
+router.post("/check-store-url", checkStoreUrl)
+router.post("/register", register)
+router.post("/login", login)
 
 
 export default router
